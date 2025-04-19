de733f879699dd0fa85a3f2ab3e48cdcb11e7691
TaJah Reynolds <tajahreynolds22@gmail.com>
2025-04-19 16:38:42 -0400

    Add profile page scaffolding

---
+++ vue-frontend/src/components/Profile.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/home">Home</RouterLink>
+    </nav>
+    <header class="profile-header flex flex-col-reverse">
+        <div class="flex justify-center gap-x-4">
+            <!-- a few icons for interaction here will vary depending on state eg add/remove, msg, block -->
+            <div class="w-10 h-10 bg-gray-300 rounded-full hover:bg-gray-500"></div>
+            <div class="w-10 h-10 bg-gray-300 rounded-full hover:bg-gray-500"></div>
+            <div class="w-10 h-10 bg-gray-300 rounded-full hover:bg-gray-500"></div>
+        </div>
+        <h1 class="text-red-600 self-center">(username) profile</h1>
+        <!-- maybe add icons for editing username and profile picture -->
+        <img
+            alt="Vue logo"
+            class="self-center"
+            src="../assets/logo.svg"
+            width="125"
+            height="125"
+        />
+    </header>
+    <section>
+        <h2 class="text-center">Showcase</h2>
+        <!-- image carousel with controls -->
+        <div class="flex overflow-x-auto gap-x-3">
+            <div v-for="n in 3" key="n" class="min-w-1/2 text-center h-80 bg-dark rounded-lg">
+                <h3 class="text-dark">Placeholder</h3>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h2 class="text-center">Collections</h2>
+        <!-- image carousel with controls -->
+        <div class="flex overflow-x-auto gap-x-3">
+            <div v-for="n in 3" key="n" class="min-w-1/2 text-center h-80 bg-dark rounded-lg">
+                <h3 class="text-dark">Placeholder</h3>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -6,11 +6,13 @@ import { createWebHistory, createRouter } from 'vue-router';
 import App from './App.vue';
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
+import Profile from './components/Profile.vue';
 import PageNotFound from './components/PageNotFound.vue';
 
 const routes = [
     { path: '/', component: Login },
     { path: '/home', component: Home },
+    { path: '/profile', component: Profile },
     { path: '/:pathmatch(.*)', component: PageNotFound },
 ];
 
